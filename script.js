4c4e865374a1266868059f3eb8eea1982280a3df
lldeveloper <ll.developer.00@gmail.com>
2025-06-07 23:02:43 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const lastName = sessionStorage.getItem('lastName');
 const dob = sessionStorage.getItem('dob');
 
-if (!lastName || !dob) {
+/**if (!lastName || !dob) {
   // No session data found, redirect to login
   window.location.href = "login.html";
 } else {
